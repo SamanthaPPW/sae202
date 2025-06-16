cd9fbeecce9c1f1377673e92baff27644ad67a1f
SamanthaPPW <samanthappw@gmail.com>
2025-06-16 14:53:11 +0000

    Creation d'un controller accueil

---
+++ controller/accueil_controller.php
@@ -0,0 +1,8 @@
+<?php
+function index(){
+  
+require('view/autres_pages/menu.php');  require('view/accueil_view.php');
+require('view/autres_pages/footer.php');
+require('view/autres_pages/header.php');
+  
+}
