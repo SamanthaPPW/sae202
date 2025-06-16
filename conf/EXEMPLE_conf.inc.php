71e8d87c3ad4c2cc5f7bdef2fb62289f208967c1
SamanthaPPW <samanthappw@gmail.com>
2025-06-16 10:09:45 +0000

    Ajout de l'exemple de conf.inc.php, Recreez le vrai fichier et ne pas le commit

---
+++ conf/EXEMPLE_conf.inc.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'NOM_DE_LA_BASE_DE_DONNEES');
+define('DB_USER', 'UTILISATEUR_DE_LA_BASE_DE_DONNEES');
+define('DB_PASSWORD', 'MDP_DE_LA_BASE_DE_DONNEES');
+?>
+
+<!--Refaire un fichier conf.inc.php sur votre machine et NE PAS le commit (c'est les mots de passe, les divulguez pas svp :)-->
