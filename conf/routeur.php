011fb5b5fb476df513b3f5cb39ee151dd7946b81
Bertone-Nambinintsoa <nambinintsoa.bertone@etudiant.univ-reims.fr>
2025-06-16 13:29:53 +0000

    Ajout controller accueil

--- conf/routeur.php
+++ conf/routeur.php
@@ -1,28 +1,9 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-<<<<<<< HEAD
 $items = explode('/', $path);
 
-if (empty($items[1])) {
-    $controller = 'accueil';
-} else {
-    $controller = $items[1];
-}
-
-if (empty($items[2])) {
-    $action = 'index';
-} else {
-    $action = $items[2];
-}
-
-require_once('controller/' . $controller . '_controller.php');
-$action();
-?>
-=======
-$item = explode('/', $path);
-
-$controller = isset($item[1]) && $item[1] !== '' ? $item[1] : 'accueil';
-$action = isset($item[2]) && $item[2] !== '' ? $item[2] : 'index';
+$controller = isset($items[1]) && $items[1] !== '' ? $items[1] : 'accueil';
+$action = isset($items[2]) && $items[2] !== '' ? $items[2] : 'index';
 
 $controllerFile = 'controller/' . $controller . '_controller.php';
 
@@ -38,4 +19,3 @@ if (file_exists($controllerFile)) {
     echo "Contrôleur '$controller' non trouvé.";
 }
 ?>
->>>>>>> fa07eac16cb1aac9c925de54a47e459fcdfd927b
