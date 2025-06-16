673b4ab247515c21632c236099744c7317522b69
MaxenceBallotWrobel <maxence.ballotwrobel@free.fr>
2025-06-16 14:21:35 +0000

    commentaire

--- conf/routeur.php
+++ conf/routeur.php
@@ -1,24 +1,5 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-<<<<<<< HEAD
-$items = explode('/', $path);
-
-if (empty($items[1])) {
-    $controller = 'accueil';
-} else {
-    $controller = $items[1];
-}
-
-if (empty($items[2])) {
-    $action = 'index';
-} else {
-    $action = $items[2];
-}
-
-require_once('controller/' . $controller . '_controller.php');
-$action();
-?>
-=======
 $item = explode('/', $path);
 
 $controller = isset($item[1]) && $item[1] !== '' ? $item[1] : 'accueil';
@@ -38,4 +19,3 @@ if (file_exists($controllerFile)) {
     echo "Contrôleur '$controller' non trouvé.";
 }
 ?>
->>>>>>> fa07eac16cb1aac9c925de54a47e459fcdfd927b
