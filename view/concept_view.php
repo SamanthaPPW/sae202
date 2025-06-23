0df2d460b7b23569fa6d695ecb5693b67fed5b99
Michel-Theo <theo.m10350@gmail.com>
2025-06-23 13:34:12 +0000

    contenu concept

--- view/concept_view.php
+++ view/concept_view.php
@@ -1,3 +1,31 @@
+<?php //A retirer plus tard
+require_once('autres_pages/menu.php');
+require_once('autres_pages/header.php');
+?>
+
 <div class="contenu">
-  
+    <div  id="murderparty">
+        <h2 class="concepttitre">Qu'est ce qu'une Murder Party</h2>
+        <p class="concepttxt">Une Murder party est un jeu de rôle grandeur nature où les participants incarnent des personnages impliqués dans une intrigue criminelle, souvent un meurtre. L’un des joueurs est le coupable, et les autres doivent enquêter, interagir et résoudre l’énigme en découvrant indices, alibis et mobiles. L’histoire se déroule dans un cadre scénarisé (manoir, croisière, etc.), et chaque participant reçoit un rôle avec des objectifs, des secrets et des relations avec les autres personnages. Le but est de résoudre le mystère… ou de ne pas se faire démasquer si l’on est le meurtrier.
+        </p>
+
+    </div>
+    <div id="pourquoi">
+       <h2 class="concepttitre">Pourquoi c'est génial</h2>
+
+        <p class="concepttxt">Une murder party, c’est génial parce que c’est une expérience immersive et ludique qui mélange enquête, théâtre, jeu de rôle et suspense. On se glisse dans la peau d’un personnage, on improvise, on manipule, on fouille les secrets des autres, tout en essayant de démêler une intrigue souvent pleine de rebondissements. C’est aussi un super moyen de vivre une aventure collective, de rire, de se creuser les méninges, et de passer un moment original entre amis ou collègues. Bref, c’est à la fois un Cluedo vivant et une pièce de théâtre dont tu es le héros !</p>
+
+    </div>
+    <div id="comment">
+       <h2 class="concepttitre">Comment ça marche?</h2>
+        <div class="ligne_concept"><img src="assets/typo.svg" alt=""><p>etape 1</p></div>
+        <div class="ligne_concept"><img src="" alt=""><p>etape 2</p></div>
+        <div class="ligne_concept"><img src="" alt=""><p>etape 3</p></div>
+
+    </div>
 </div>
+
+<?php //A retirer plus tard
+require_once('autres_pages/footer.php');
+
+?>
