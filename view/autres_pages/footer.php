8c539b00a076be3bb745a80d1d6d4ebdc7f7b4d6
Bertone-Nambinintsoa <nambinintsoa.bertone@etudiant.univ-reims.fr>
2025-06-23 14:29:53 +0000

    FOOTER OP

--- view/autres_pages/footer.php
+++ view/autres_pages/footer.php
@@ -1,3 +1,21 @@
-<a href="/mentions">Mentions légales</a>
+<footer class="footer">
+    <div class="footer-links">
+        <a href="/mentions">Mentions légales</a>
+        <a href="#">Accessibilité</a>
+        <a href="#">Plan du site</a>
+    </div>
+
+    <div class="footer-logos">
+        <a href="https://instagram.com" target="_blank">
+            <img src="view/assets/picto_insta.svg" alt="Instagram" class="footer-icon">
+        </a>
+        <a href="https://tiktok.com" target="_blank">
+            <img src="view/assets/picto_tiktok.svg" alt="TikTok" class="footer-icon">
+        </a>
+        <a href="https://facebook.com" target="_blank">
+            <img src="view/assets/picto_facebook.svg" alt="Facebook" class="footer-icon">
+        </a>
+    </div>
+</footer>
 </body>
 </html>
