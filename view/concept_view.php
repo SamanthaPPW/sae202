8c12cdccc75b69483f4a15792fbbe4e723629a63
Michel-Theo <theo.m10350@gmail.com>
2025-06-24 12:52:36 +0000

    page concept + css

--- view/concept_view.php
+++ view/concept_view.php
@@ -1,19 +1,22 @@
+
+
+<link rel="stylesheet" href="concept_view.css">
+
 <?php //A retirer plus tard
 require_once('autres_pages/menu.php');
 require_once('autres_pages/header.php');
 ?>
-<link rel="stylesheet" href="concept_view.css">
 
 <div class="contenu_concept">
     <div  id="murderparty">
         <div class="section-inner">
             <div class="section-flex">
                 <div class="section-flex-left">
-                    <h2 class="concepttitre">Qu'est ce qu'une Murder Party</h2>
-                    <img class="img-section" src="https://placehold.co/454x280" alt="murder party placeholder">
+                    <h1 class="concepttitre">Qu'est ce qu'une Murder Party</h1>
+                    <p class="concepttxt">Une Murder Party, c’est un jeu de rôle grandeur nature où les participants incarnent des personnages liés à une enquête criminelle. L’un d’eux est le meurtrier, les autres mènent l’enquête. Chacun a un rôle, des secrets, des objectifs… et un mystère à élucider ou à dissimuler.</p>
                 </div>
                 <div class="section-flex-right">
-                    <p class="concepttxt">Une Murder party est un jeu de rôle grandeur nature où les participants incarnent des personnages impliqués dans une intrigue criminelle, souvent un meurtre. L'un des joueurs est le coupable, et les autres doivent enquêter, interagir et résoudre l'énigme en découvrant indices, alibis et mobiles. L'histoire se déroule dans un cadre scénarisé (manoir, croisière, etc.), et chaque participant reçoit un rôle avec des objectifs, des secrets et des relations avec les autres personnages. Le but est de résoudre le mystère… ou de ne pas se faire démasquer si l'on est le meurtrier.</p>
+                    <img class="img-section" src="https://placehold.co/454x280" alt="murder party placeholder">
                 </div>
             </div>
         </div>
@@ -22,21 +25,29 @@ require_once('autres_pages/header.php');
         <div class="section-inner">
             <div class="section-flex">
                 <div class="section-flex-left">
-                    <h2 class="concepttitre">Pourquoi c'est génial</h2>
-                    <img class="img-section" src="https://placehold.co/454x280" alt="Pourquoi placeholder">
+                    <h1 class="concepttitre">Pourquoi c'est génial</h1>
+                    <p class="concepttxt">Une murder party, c’est une aventure immersive mêlant enquête, jeu de rôle, théâtre et suspense. On incarne un personnage, on improvise, on manipule, on enquête… le tout dans une intrigue pleine de rebondissements. C’est un Cluedo vivant et une pièce de théâtre dont tu es le héros. Fun, original et parfait à vivre entre amis ou collègues !</p>
                 </div>
                 <div class="section-flex-right">
-                    <p class="concepttxt">Une murder party, c'est génial parce que c'est une expérience immersive et ludique qui mélange enquête, théâtre, jeu de rôle et suspense. On se glisse dans la peau d'un personnage, on improvise, on manipule, on fouille les secrets des autres, tout en essayant de démêler une intrigue souvent pleine de rebondissements. C'est aussi un super moyen de vivre une aventure collective, de rire, de se creuser les méninges, et de passer un moment original entre amis ou collègues. Bref, c'est à la fois un Cluedo vivant et une pièce de théâtre dont tu es le héros !</p>
+                    <img class="img-section" src="https://placehold.co/454x280" alt="Pourquoi placeholder">
                 </div>
             </div>
         </div>
     </div>
     <div id="comment">
         <div class="section-inner">
-            <h2 class="concepttitre">Comment ça marche?</h2>
-            <div class="ligne_concept"><img src="assets/typo.svg" alt=""><p>etape 1</p></div>
-            <div class="ligne_concept"><img src="" alt=""><p>etape 2</p></div>
-            <div class="ligne_concept"><img src="" alt=""><p>etape 3</p></div>
+            <div class="section-flex">
+                <div id="comment_titre">
+                    <h1 class="concepttitre">Comment ça marche?</h1>
+                </div>
+                <div id="comment_ligne">
+                    <div class="ligne_concept"><img src="assets/typo.svg" alt=""><p class="concepttxt">Plongez dans l'univers</p></div>
+                    <div class="ligne_concept"><img src="" alt=""><p class="concepttxt">Menez l'enquête</p></div>
+                    <div class="ligne_concept"><img src="" alt=""><p class="concepttxt">Démasquez le coupable</p></div>
+                </div>
+
+            </div>
+        </div>
         </div>
     </div>
 </div>
